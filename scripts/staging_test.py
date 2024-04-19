7a1ca94c2a1ed857f0ba5d619c9ad27ccc4d7fd4
Qian Li <qian.li@dbos.dev>
2024-04-18 17:12:42 -0700

    register user

--- scripts/staging_test.py
+++ scripts/staging_test.py
@@ -1,15 +1,19 @@
 # This script is used to automatically deploy this subscription app to DBOS Cloud
 import os
-from utils import (login)
+from utils import (login, run_subprocess)
+from config import config
 
 script_dir = os.path.dirname(os.path.abspath(__file__))
 app_dir = os.path.join(script_dir, "..")
 
 
 def test_endpoints(path: str):
-    #Test endpoint
     print("Testing endpoints on DBOS Cloud...")
-
+    # Register on DBOS Cloud
+    run_subprocess(['npx', 'dbos-cloud', 'register', '-u', config.test_username], path, check=False)
+    login(path, is_deploy=False) # Login again because register command logs out
+    output = run_subprocess(['npx', 'dbos-cloud', 'db', 'list'], path, check=False)
+    print(output)
 
 if __name__ == "__main__":
     login(app_dir, is_deploy=False)

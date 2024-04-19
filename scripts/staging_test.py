5f72559e4b3cfc6224a10fd4e427380ee4af4ba0
Qian Li <qian.li@dbos.dev>
2024-04-19 11:21:07 -0700

    test cancel subscription

--- scripts/staging_test.py
+++ scripts/staging_test.py
@@ -1,5 +1,6 @@
 # This script is used to automatically deploy this subscription app to DBOS Cloud
 import os
+import time
 from utils import (login, run_subprocess)
 from config import config
 import stripe
@@ -13,24 +14,36 @@ def test_endpoints(path: str):
     # Register on DBOS Cloud
     run_subprocess(['npx', 'dbos-cloud', 'register', '-u', config.test_username], path, check=False)
     login(path, is_deploy=False) # Login again because register command logs out
+
+    # Test database linking, should fail because we're free user
     run_subprocess(['npx', 'dbos-cloud', 'db', 'list'], path, check=True)
     output = run_subprocess(['npx', 'dbos-cloud', 'db', 'link', 'testlinkdb', '-H', 'localhost', '-W', 'fakepassword'], path, check=False)
     # TODO: add a real user subscription status check.
     if not "database linking is only available for paying users" in output:
         raise Exception("Free tier check failed")
 
-    # Now, create a subscription through stripe
+    # Look up customer ID
     customers = stripe.Customer.list(email=config.test_email, limit=1)
     if len(customers) == 0:
         raise Exception("No Stripe customer found for test email")
     customer_id = customers.data[0].id
-    print(f"Customer ID: {customer_id}")
     
+    # Create a subscription that uses the default test payment
     subscription = stripe.Subscription.create(
         customer=customer_id,
         items=[{"price": config.stripe_pro_price}],
     )
-    print(f"Subscription ID: {subscription.id}")
+
+    # Now test linking a database should fail with a different message
+    # TODO: better check
+    time.sleep(5) # Wait for subscription to take effect
+    output = run_subprocess(['npx', 'dbos-cloud', 'db', 'link', 'testlinkdb', '-H', 'localhost', '-W', 'fakepassword'], path, check=False)
+    if not "failed to connect to linked database" in output:
+        raise Exception("Pro tier check failed")
+
+    # Cancel the subscription
+    stripe.Subscription.cancel(subscription.id)
+    time.sleep(5) # Wait for subscription to take effect
 
 if __name__ == "__main__":
     login(app_dir, is_deploy=False)

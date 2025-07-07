a1c71bbe325823afccd51aa7f1ace01cb0e4e2cd
Qian Li <qian.li@dbos.dev>
2025-07-07 16:49:25 -0700

    upgrade to TSV3, make migration work

--- knexfile.js
+++ knexfile.js
@@ -1,17 +1,6 @@
-const { parseConfigFile } = require('@dbos-inc/dbos-sdk/dist/src/dbos-runtime/config');
-const { DBOSConfig } = require('@dbos-inc/dbos-sdk/dist/src/dbos-executor');
-
-const [dbosConfig, ] = parseConfigFile();
-
 const config = {
   client: 'pg',
-  connection: {
-    host: dbosConfig.poolConfig.host,
-    user: dbosConfig.poolConfig.user,
-    password: dbosConfig.poolConfig.password,
-    database: dbosConfig.poolConfig.database,
-    ssl: dbosConfig.poolConfig.ssl,
-  },
+  connection: process.env.DBOS_DATABASE_URL || `postgresql://postgres:${process.env.PGPASSWORD || 'dbos'}@localhost:5432/cloud_account`,
   migrations: {
     directory: './migrations'
   }

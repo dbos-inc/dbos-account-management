7e0e0a27699c5f2adbd85ce10f55f526212cdc4e
Qian Li <qian.li@dbos.dev>
2025-07-09 15:06:27 -0700

    CORS tests

--- src/subscription.test.ts
+++ src/subscription.test.ts
@@ -67,48 +67,88 @@ describe('subscription-tests', () => {
     ); // Non existent user
   });
 
-  // test("subscribe-cors", async () => {
-  //   // Check the prefligth request has the correct CORS headers
-  //   const resp = await request(testRuntime.getHandlersCallback())
-  //     .options("/subscribe")
-  //     .set("Origin", "https://dbos.dev")
-  //     .set("Access-Control-Request-Method", "POST")
-  //     .set("Authorization", "Bearer testtoken");
-  //   expect(resp.status).toBe(204);
-  //   expect(resp.headers["access-control-allow-origin"]).toBe("https://dbos.dev");
-  //   expect(resp.headers["access-control-allow-credentials"]).toBe("true");
+  test('subscribe-cors', async () => {
+    // Check the prefligth request has the correct CORS headers
+    fastify.inject(
+      {
+        method: 'OPTIONS',
+        url: '/subscribe',
+        headers: {
+          Origin: 'https://dbos.dev',
+          'Access-Control-Request-Method': 'POST',
+          Authorization: 'Bearer testtoken',
+        },
+      },
+      (err, resp) => {
+        expect(err).toBeFalsy();
+        expect(resp).toBeDefined();
+        expect(resp!.statusCode).toBe(204);
+        expect(resp!.headers['access-control-allow-origin']).toBe('https://dbos.dev');
+        expect(resp!.headers['access-control-allow-credentials']).toBe('true');
+      },
+    );
 
-  //   // Our staging env.
-  //   const resp2 = await request(testRuntime.getHandlersCallback())
-  //     .options("/subscribe")
-  //     .set("Origin", "https://dbos.webflow.io")
-  //     .set("Access-Control-Request-Method", "POST")
-  //     .set("Authorization", "Bearer testtoken");
-  //   expect(resp2.status).toBe(204);
-  //   expect(resp2.headers["access-control-allow-origin"]).toBe("https://dbos.webflow.io");
-  //   expect(resp2.headers["access-control-allow-credentials"]).toBe("true");
+    // Our staging env.
+    fastify.inject(
+      {
+        method: 'OPTIONS',
+        url: '/subscribe',
+        headers: {
+          Origin: 'https://dbos.webflow.io',
+          'Access-Control-Request-Method': 'POST',
+          Authorization: 'Bearer testtoken',
+        },
+      },
+      (err, resp) => {
+        expect(err).toBeFalsy();
+        expect(resp).toBeDefined();
+        expect(resp!.statusCode).toBe(204);
+        expect(resp!.headers['access-control-allow-origin']).toBe('https://dbos.webflow.io');
+        expect(resp!.headers['access-control-allow-credentials']).toBe('true');
+      },
+    );
 
-  //   // Cloud console
-  //   const resp3 = await request(testRuntime.getHandlersCallback())
-  //     .options("/subscribe")
-  //     .set("Origin", "https://console.dbos.dev")
-  //     .set("Access-Control-Request-Method", "POST")
-  //     .set("Authorization", "Bearer testtoken");
-  //   expect(resp3.status).toBe(204);
-  //   expect(resp3.headers["access-control-allow-origin"]).toBe("https://console.dbos.dev");
-  //   expect(resp3.headers["access-control-allow-credentials"]).toBe("true");
+    // Cloud console
+    fastify.inject(
+      {
+        method: 'OPTIONS',
+        url: '/subscribe',
+        headers: {
+          Origin: 'https://console.dbos.dev',
+          'Access-Control-Request-Method': 'POST',
+          Authorization: 'Bearer testtoken',
+        },
+      },
+      (err, resp) => {
+        expect(err).toBeFalsy();
+        expect(resp).toBeDefined();
+        expect(resp!.statusCode).toBe(204);
+        expect(resp!.headers['access-control-allow-origin']).toBe('https://console.dbos.dev');
+        expect(resp!.headers['access-control-allow-credentials']).toBe('true');
+      },
+    );
 
-  //   const resp4 = await request(testRuntime.getHandlersCallback())
-  //     .options("/subscribe")
-  //     .set("Origin", "https://staging.console.dbos.dev")
-  //     .set("Access-Control-Request-Method", "POST")
-  //     .set("Authorization", "Bearer testtoken");
-  //   expect(resp4.status).toBe(204);
-  //   expect(resp4.headers["access-control-allow-origin"]).toBe("https://staging.console.dbos.dev");
-  //   expect(resp4.headers["access-control-allow-credentials"]).toBe("true");
-  // });
+    fastify.inject(
+      {
+        method: 'OPTIONS',
+        url: '/subscribe',
+        headers: {
+          Origin: 'https://staging.console.dbos.dev',
+          'Access-Control-Request-Method': 'POST',
+          Authorization: 'Bearer testtoken',
+        },
+      },
+      (err, resp) => {
+        expect(err).toBeFalsy();
+        expect(resp).toBeDefined();
+        expect(resp!.statusCode).toBe(204);
+        expect(resp!.headers['access-control-allow-origin']).toBe('https://staging.console.dbos.dev');
+        expect(resp!.headers['access-control-allow-credentials']).toBe('true');
+      },
+    );
+  });
 
-  // // Test retrieve cloud credentials
+  // Test retrieve cloud credentials
   // test("cloud-credential", async () => {
   //   if (!process.env.DBOS_LOGIN_REFRESH_TOKEN || process.env.DBOS_LOGIN_REFRESH_TOKEN == "null") {
   //     console.log("Skipping cloud-credentials test, no refresh token provided");

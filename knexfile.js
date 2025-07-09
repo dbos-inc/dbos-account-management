25b65ac737ac729849ac28d4a47183ca66e9039f
Qian Li <qian.li@dbos.dev>
2025-07-08 19:16:26 -0700

    fix default URL

--- knexfile.js
+++ knexfile.js
@@ -1,6 +1,6 @@
 const config = {
   client: 'pg',
-  connection: process.env.DBOS_DATABASE_URL || `postgresql://postgres:${process.env.PGPASSWORD || 'dbos'}@localhost:5432/cloud_account`,
+  connection: process.env.DBOS_DATABASE_URL || `postgresql://postgres:${process.env.PGPASSWORD || 'dbos'}@localhost:5432/dbos`,
   migrations: {
     directory: './migrations'
   }
